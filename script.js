1f174e6033685e00ac1c557205d9958b3151211c
MaulanaHidayatulloh <ulyadbaim@gmail.com>
2024-04-01 17:46:06 +0700

    perubahan commit keenam

--- script.js
+++ script.js
@@ -1,105 +1,154 @@
-// ===> Bagian Pubertas <===
-// #1
-const Pubertasbawah1 = document.querySelector(".bawah-pubertas-1");
-const Pubertaskanan1 = document.querySelector(".kanan-pubertas-1");
-const Pubertasketerangan1 = document.querySelector(".keterangan-pubertas-1");
+document.addEventListener("DOMContentLoaded", function () {
+  // ===> Bagian Home (Main Content) <===
+  // #1
+  const Maincontent1 = document.querySelector(".main-content-1");
+  const Mainp1 = document.querySelector(".main-p-1");
+  const Maincontinue1 = document.querySelector(".main-continue-1");
+  const Mainclose1 = document.querySelector(".main-close-1");
 
-// ketika klik panah kanan
-Pubertaskanan1.onclick = (e) => {
-  Pubertasbawah1.classList.toggle("active");
-  Pubertaskanan1.classList.toggle("active");
-  Pubertasketerangan1.classList.toggle("active");
-  e.preventDefault();
-};
+  // ketika klik pelajari selengkapnya
+  Maincontinue1.onclick = (e) => {
+    Mainp1.classList.toggle("active");
+    Maincontent1.classList.toggle("active");
+    Maincontinue1.classList.toggle("active");
+    e.preventDefault();
+  };
 
-// ketika klik panah bawah
-Pubertasbawah1.onclick = (e) => {
-  Pubertasbawah1.classList.toggle("active");
-  Pubertaskanan1.classList.toggle("active");
-  Pubertasketerangan1.classList.toggle("active");
-  e.preventDefault();
-};
+  // ketika klik tutup
+  Mainclose1.onclick = (e) => {
+    Mainp1.classList.toggle("active");
+    Maincontent1.classList.toggle("active");
+    Maincontinue1.classList.toggle("active");
+    e.preventDefault();
+  };
 
-// #2
-const Pubertasbawah2 = document.querySelector(".bawah-pubertas-2");
-const Pubertaskanan2 = document.querySelector(".kanan-pubertas-2");
-const Pubertasketerangan2 = document.querySelector(".keterangan-pubertas-2");
+  // #2
+  const Maincontent2 = document.querySelector(".main-content-2");
+  const Mainp2 = document.querySelector(".main-p-2");
+  const Maincontinue2 = document.querySelector(".main-continue-2");
+  const Mainclose2 = document.querySelector(".main-close-2");
 
-// ketika klik panah kanan
-Pubertaskanan2.onclick = (e) => {
-  Pubertasbawah2.classList.toggle("active");
-  Pubertaskanan2.classList.toggle("active");
-  Pubertasketerangan2.classList.toggle("active");
-  e.preventDefault();
-};
+  // ketika klik pelajari selengkapnya
+  Maincontinue2.onclick = (e) => {
+    Mainp2.classList.toggle("active");
+    Maincontent2.classList.toggle("active");
+    Maincontinue2.classList.toggle("active");
+    e.preventDefault();
+  };
 
-// ketika klik panah bawah
-Pubertasbawah2.onclick = (e) => {
-  Pubertasbawah2.classList.toggle("active");
-  Pubertaskanan2.classList.toggle("active");
-  Pubertasketerangan2.classList.toggle("active");
-  e.preventDefault();
-};
+  // ketika klik tutup
+  Mainclose2.onclick = (e) => {
+    Mainp2.classList.toggle("active");
+    Maincontent2.classList.toggle("active");
+    Maincontinue2.classList.toggle("active");
+    e.preventDefault();
+  };
+});
 
-// #3
-const Pubertasbawah3 = document.querySelector(".bawah-pubertas-3");
-const Pubertaskanan3 = document.querySelector(".kanan-pubertas-3");
-const Pubertasketerangan3 = document.querySelector(".keterangan-pubertas-3");
+document.addEventListener("DOMContentLoaded", function () {
+  // ===> Bagian Pubertas <===
+  // #1
+  const Pubertasbawah1 = document.querySelector(".bawah-pubertas-1");
+  const Pubertaskanan1 = document.querySelector(".kanan-pubertas-1");
+  const Pubertasketerangan1 = document.querySelector(".keterangan-pubertas-1");
 
-// ketika klik panah kanan
-Pubertaskanan3.onclick = (e) => {
-  Pubertasbawah3.classList.toggle("active");
-  Pubertaskanan3.classList.toggle("active");
-  Pubertasketerangan3.classList.toggle("active");
-  e.preventDefault();
-};
+  // ketika klik panah kanan
+  Pubertaskanan1.onclick = (e) => {
+    Pubertasbawah1.classList.toggle("active");
+    Pubertaskanan1.classList.toggle("active");
+    Pubertasketerangan1.classList.toggle("active");
+    e.preventDefault();
+  };
 
-// ketika klik panah bawah
-Pubertasbawah3.onclick = (e) => {
-  Pubertasbawah3.classList.toggle("active");
-  Pubertaskanan3.classList.toggle("active");
-  Pubertasketerangan3.classList.toggle("active");
-  e.preventDefault();
-};
+  // ketika klik panah bawah
+  Pubertasbawah1.onclick = (e) => {
+    Pubertasbawah1.classList.toggle("active");
+    Pubertaskanan1.classList.toggle("active");
+    Pubertasketerangan1.classList.toggle("active");
+    e.preventDefault();
+  };
 
-// #4
-const Pubertasbawah4 = document.querySelector(".bawah-pubertas-4");
-const Pubertaskanan4 = document.querySelector(".kanan-pubertas-4");
-const Pubertasketerangan4 = document.querySelector(".keterangan-pubertas-4");
+  // #2
+  const Pubertasbawah2 = document.querySelector(".bawah-pubertas-2");
+  const Pubertaskanan2 = document.querySelector(".kanan-pubertas-2");
+  const Pubertasketerangan2 = document.querySelector(".keterangan-pubertas-2");
 
-// ketika klik panah kanan
-Pubertaskanan4.onclick = (e) => {
-  Pubertasbawah4.classList.toggle("active");
-  Pubertaskanan4.classList.toggle("active");
-  Pubertasketerangan4.classList.toggle("active");
-  e.preventDefault();
-};
+  // ketika klik panah kanan
+  Pubertaskanan2.onclick = (e) => {
+    Pubertasbawah2.classList.toggle("active");
+    Pubertaskanan2.classList.toggle("active");
+    Pubertasketerangan2.classList.toggle("active");
+    e.preventDefault();
+  };
 
-// ketika klik panah bawah
-Pubertasbawah4.onclick = (e) => {
-  Pubertasbawah4.classList.toggle("active");
-  Pubertaskanan4.classList.toggle("active");
-  Pubertasketerangan4.classList.toggle("active");
-  e.preventDefault();
-};
+  // ketika klik panah bawah
+  Pubertasbawah2.onclick = (e) => {
+    Pubertasbawah2.classList.toggle("active");
+    Pubertaskanan2.classList.toggle("active");
+    Pubertasketerangan2.classList.toggle("active");
+    e.preventDefault();
+  };
 
-// #5
-const Pubertasbawah5 = document.querySelector(".bawah-pubertas-5");
-const Pubertaskanan5 = document.querySelector(".kanan-pubertas-5");
-const Pubertasketerangan5 = document.querySelector(".keterangan-pubertas-5");
+  // #3
+  const Pubertasbawah3 = document.querySelector(".bawah-pubertas-3");
+  const Pubertaskanan3 = document.querySelector(".kanan-pubertas-3");
+  const Pubertasketerangan3 = document.querySelector(".keterangan-pubertas-3");
 
-// ketika klik panah kanan
-Pubertaskanan5.onclick = (e) => {
-  Pubertasbawah5.classList.toggle("active");
-  Pubertaskanan5.classList.toggle("active");
-  Pubertasketerangan5.classList.toggle("active");
-  e.preventDefault();
-};
+  // ketika klik panah kanan
+  Pubertaskanan3.onclick = (e) => {
+    Pubertasbawah3.classList.toggle("active");
+    Pubertaskanan3.classList.toggle("active");
+    Pubertasketerangan3.classList.toggle("active");
+    e.preventDefault();
+  };
 
-// ketika klik panah bawah
-Pubertasbawah5.onclick = (e) => {
-  Pubertasbawah5.classList.toggle("active");
-  Pubertaskanan5.classList.toggle("active");
-  Pubertasketerangan5.classList.toggle("active");
-  e.preventDefault();
-};
+  // ketika klik panah bawah
+  Pubertasbawah3.onclick = (e) => {
+    Pubertasbawah3.classList.toggle("active");
+    Pubertaskanan3.classList.toggle("active");
+    Pubertasketerangan3.classList.toggle("active");
+    e.preventDefault();
+  };
+
+  // #4
+  const Pubertasbawah4 = document.querySelector(".bawah-pubertas-4");
+  const Pubertaskanan4 = document.querySelector(".kanan-pubertas-4");
+  const Pubertasketerangan4 = document.querySelector(".keterangan-pubertas-4");
+
+  // ketika klik panah kanan
+  Pubertaskanan4.onclick = (e) => {
+    Pubertasbawah4.classList.toggle("active");
+    Pubertaskanan4.classList.toggle("active");
+    Pubertasketerangan4.classList.toggle("active");
+    e.preventDefault();
+  };
+
+  // ketika klik panah bawah
+  Pubertasbawah4.onclick = (e) => {
+    Pubertasbawah4.classList.toggle("active");
+    Pubertaskanan4.classList.toggle("active");
+    Pubertasketerangan4.classList.toggle("active");
+    e.preventDefault();
+  };
+
+  // #5
+  const Pubertasbawah5 = document.querySelector(".bawah-pubertas-5");
+  const Pubertaskanan5 = document.querySelector(".kanan-pubertas-5");
+  const Pubertasketerangan5 = document.querySelector(".keterangan-pubertas-5");
+
+  // ketika klik panah kanan
+  Pubertaskanan5.onclick = (e) => {
+    Pubertasbawah5.classList.toggle("active");
+    Pubertaskanan5.classList.toggle("active");
+    Pubertasketerangan5.classList.toggle("active");
+    e.preventDefault();
+  };
+
+  // ketika klik panah bawah
+  Pubertasbawah5.onclick = (e) => {
+    Pubertasbawah5.classList.toggle("active");
+    Pubertaskanan5.classList.toggle("active");
+    Pubertasketerangan5.classList.toggle("active");
+    e.preventDefault();
+  };
+});
